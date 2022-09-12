c98c5535c3cc0f211a04cc1daf6af2c79dbb39bf
Amaya <bryanta21@nku.edu>
2022-09-12 19:26:49 -0400

    added equation in comments, also added in max values

--- Laptop.java
+++ Laptop.java
@@ -5,18 +5,32 @@ Assignment: 2 part 2
 Purpose: learning how to implement cloneable and comparable
 Course: OOP2 - CSC 360
 */
+import java.util.Random;
 public class Laptop implements Comparable<Laptop>{
     
-    private int ram;
-    private int hdd;
-    private int weight;
-    private int battery;
-    private int price;
+    private double ram;
+    private double hdd;
+    private double weight;
+    private double battery;
+    private double price;
     private double cpu;
     private double screen;
     private boolean graphics;
+    private double laptopScore;
 
-    protected Laptop(){
+    final double ramMax = 32;
+    final double hddMax = 2048;
+    final double weightMax = 6;
+    final double batteryMax = 9;
+    final double priceMax = 2000;
+    final double cpuMax = 3.0;
+    final double screenMax = 17.0; 
+
+    public Laptop(){ 
+        Random r = new Random();
+        
+
+        //laptopScore = (2 * cpu / cpuMax) + (2 * ram / ramMax) + (1 * hdd / hddMax) + (graphics) + (1 * screen / screenMax) + (1 * weight / weightMax) + (1 * battery / batteryMax) + (1 * price / priceMax);
 
     }
     
